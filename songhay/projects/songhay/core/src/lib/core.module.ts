4f160a3e4ea5ebc60cf6aa67e0321ad1d71bae5f
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-03-16 20:12:35 -0700

    :hammer: :pencil2: updated module, issue #8

--- songhay/projects/songhay/core/src/lib/core.module.ts
+++ songhay/projects/songhay/core/src/lib/core.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 
 import { AppDataService } from './services/app-data.service';
+import { AppDataStore } from './services/app-data.store';
+import { AppDataStoreOptions } from './services/app-data-store.options';
 import { DomSanitizerUtility } from './services/dom-sanitizer.utility';
 
 @NgModule({
-    providers: [AppDataService, DomSanitizerUtility]
+    providers: [
+        AppDataService,
+        AppDataStore,
+        AppDataStoreOptions,
+        DomSanitizerUtility
+    ]
 })
 export class CoreModule { }
--- songhay/projects/songhay/core/src/public_api.ts
+++ songhay/projects/songhay/core/src/public_api.ts
@@ -6,6 +6,9 @@ export * from './lib/core.module';
 
 export * from './lib/mocks/activated-route-mock';
 
+export * from './lib/models/http-client-options';
+export * from './lib/models/send-methods.type';
+
 export * from './lib/services/app-data.service';
 export * from './lib/services/app-data-store.options';
 export * from './lib/services/dom-sanitizer.utility';
