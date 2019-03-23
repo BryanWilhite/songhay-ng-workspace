c8ec39b53160eb7ce3cea74a09ffba12fcd7106f
Bryan Wilhite <rasx@kintespace.com>
2019-03-23 15:49:30 -0700

    :pencil2: filled out YouTubeItemDataStore

--- songhay/projects/songhay/player-video-you-tube/src/lib/services/you-tube-channel-data.store.ts
+++ songhay/projects/songhay/player-video-you-tube/src/lib/services/you-tube-channel-data.store.ts
@@ -1,4 +1,66 @@
-import { AppDataStore } from '@songhay/core';
-import { YouTubeItem } from '@songhay/player-video-you-tube';
+import { AppDataStore, AppDataStoreOptions } from '@songhay/core';
+import { YouTubeItem, YouTubeScalars } from '@songhay/player-video-you-tube';
+import { SendMethods } from 'projects/songhay/core/src/public_api';
 
-export class YouTubeItemDataStore extends AppDataStore<YouTubeItem, any> {}
+/**
+ * Stores YouTube Channel item data, curated by the Songhay studio.
+ *
+ * @export
+ * @class YouTubeItemDataStore
+ * @extends {AppDataStore<YouTubeItem[], any>}
+ */
+export class YouTubeItemDataStore extends AppDataStore<YouTubeItem[], any> {
+    /**
+     * returns @type {AppDataStoreOptions<YouTubeItem[], any>}
+     * for this store
+     *
+     * @readonly
+     * @static
+     * @type {AppDataStoreOptions<YouTubeItem[], any>}
+     * @memberof YouTubeItemDataStore
+     */
+    static get options(): AppDataStoreOptions<YouTubeItem[], any> {
+        return {
+            domainConverter: (method, data) => {
+                switch (method) {
+                    default:
+                    case 'get':
+                        return YouTubeItemDataStore.getItems(data);
+                }
+            }
+        };
+    }
+
+    /**
+     * gets @type {YouTubeItem[]}
+     * from JSON of the shape { items: [] } }
+     *
+     * @static
+     * @param {{}} json
+     * @returns {YouTubeItem[]}
+     * @memberof YouTubeDataService
+     */
+    static getItems(json: {}): YouTubeItem[] {
+        const items = json['items'] as YouTubeItem[];
+        return items;
+    }
+
+    /**
+     * gets the URI based on the specified @type {SendMethods}
+     *
+     * @static
+     * @param {SendMethods} method
+     * @param {string} [channelId]
+     * @returns {string}
+     * @memberof YouTubeItemDataStore
+     */
+    static getUri(method: SendMethods, channelId?: string): string {
+        switch (method) {
+            default:
+            case 'get':
+                return `${YouTubeScalars.rxYouTubeApiRootUri}${
+                    YouTubeScalars.rxYouTubeApiPlaylistPath
+                }${channelId}`;
+        }
+    }
+}
