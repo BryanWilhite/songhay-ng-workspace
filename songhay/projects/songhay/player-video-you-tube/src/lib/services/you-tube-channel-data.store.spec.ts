ac436791ef43a2476a7552f04b6ecfa29b0dac7e
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-03-25 10:43:59 -0700

    :snowflake: :umbrella: learned how to import JSON files directly for mocks, issue #10

---
+++ songhay/projects/songhay/player-video-you-tube/src/lib/services/you-tube-channel-data.store.spec.ts
@@ -0,0 +1,6 @@
+import * as videos from '../mocks/data/video-yt-bowie0-videos.json';
+import { YouTubeChannelDataStore } from './you-tube-channel-data.store.js';
+
+describe(`${YouTubeChannelDataStore.name}`, () => {
+    console.log('yup', videos.items);
+});
