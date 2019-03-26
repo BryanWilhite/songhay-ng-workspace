225867cbf82605a55391c2e4c9a2176d76677e06
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-03-26 09:49:39 -0700

    :umbrella: stupped out endpoint spec, issue #10

--- songhay/projects/songhay/player-video-you-tube/src/lib/services/you-tube-channel-data.store.spec.ts
+++ songhay/projects/songhay/player-video-you-tube/src/lib/services/you-tube-channel-data.store.spec.ts
@@ -7,13 +7,16 @@ import * as videos from '../mocks/data/video-yt-bowie0-videos.json';
 
 describe(`${YouTubeChannelDataStore.name}`, () => {
     console.log('yup', videos.items);
+    const getOptionsMethodName = 'getOptions';
+    let spyOnGetOptions: jasmine.Spy;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
-            providers: [
-                YouTubeChannelDataStore
-            ]
+            providers: [YouTubeChannelDataStore]
         });
+
+        spyOnGetOptions = spyOn(YouTubeChannelDataStore, getOptionsMethodName);
     });
 
     it('should instantiate', inject(
@@ -22,4 +25,15 @@ describe(`${YouTubeChannelDataStore.name}`, () => {
             expect(service).toBeTruthy();
         }
     ));
+
+    it(`should call ${getOptionsMethodName} once`, inject(
+        [YouTubeChannelDataStore],
+        (service: YouTubeChannelDataStore) => {
+            expect(spyOnGetOptions).toHaveBeenCalledTimes(1);
+        }
+    ));
+
+    const endpointMethod = 'get';
+    const endpoint = YouTubeChannelDataStore.getUri(endpointMethod, '{suffix}');
+    describe(`endpoint: ${endpoint}`, () => {});
 });
