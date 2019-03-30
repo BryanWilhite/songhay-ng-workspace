7ae66159c063e86c9579398c471099a3610ffbb7
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-03-30 12:57:46 -0700

    :tractor: :lipstick: added angular material properly, issue #10

--- songhay/projects/client/src/app/components/app.component.ts
+++ songhay/projects/client/src/app/components/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'workspace client for libraries';
+  title = 'songhay-ng-workspace';
 }
