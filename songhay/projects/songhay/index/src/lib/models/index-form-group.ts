028271c66444ddfcb055d70bf0f49e729ce064a6
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-04-01 16:55:36 -0700

    :hammer: :pencil2: IndexEntriesStore feels feature complete issue #11

--- songhay/projects/songhay/index/src/lib/models/index-form-group.ts
+++ songhay/projects/songhay/index/src/lib/models/index-form-group.ts
@@ -1,6 +1,6 @@
 import { IndexGroupingOption } from './index-grouping-option';
 
-export interface IIndexFormGroup {
+export interface IndexFormGroup {
     indexGroupingSelection: IndexGroupingOption;
     indexFilter: string;
 }
--- songhay/projects/songhay/index/src/lib/services/index-entries.store.ts
+++ songhay/projects/songhay/index/src/lib/services/index-entries.store.ts
@@ -1,3 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import { DisplayItemModel } from 'songhay/core/models/display-item.model';
+
 import { AppDataStore } from '@songhay/core';
 
-export class IndexEntriesStore extends AppDataStore<any, any> {}
+/**
+ * stores Index entries
+ *
+ * @export
+ */
+@Injectable()
+export class IndexEntriesStore extends AppDataStore<DisplayItemModel[], any> {
+    /**
+     * filters @type {DisplayItemModel} entries
+     *
+     */
+    static filterEntries(entries: DisplayItemModel[], particle: string): DisplayItemModel[] {
+        const contains = (needle: string, haystack: string) => {
+            return (
+                needle &&
+                haystack &&
+                haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1
+            );
+        };
+        if (!particle) {
+            return entries;
+        }
+        if (particle.length < 2) {
+            return entries;
+        }
+        return entries.filter(i => contains(particle, i.displayText));
+    }
+}
