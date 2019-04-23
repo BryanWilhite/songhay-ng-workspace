90285a80a6b2ca282b1f109305f265425f72ffc8
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-04-22 21:20:38 -0700

    :pencil2: issue #27

--- songhay/projects/client/src/app/app-routing.module.ts
+++ songhay/projects/client/src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { YouTubeRoutePaths } from '@songhay/player-video-you-tube';
 import { MockBlogEntryComponent } from './components/index-app/mock-blog.entry.component';
 
 const routes: Routes = [
     { path: 'index-app/blog/entry/:id', component: MockBlogEntryComponent },
     {
         path: 'yt',
-        loadChildren: './components/yt/you-tube-lib.module#YouTubeLibModule'
-    },
-    {
-        path: `${YouTubeRoutePaths.root}${YouTubeRoutePaths.uploads}`,
         loadChildren: './components/yt/you-tube-lib.module#YouTubeLibModule'
     },
     {
